267337f77f613741c9d10e518ed9b2b3c6e99728
Serge De Guzman <sergetrever@gmail.com>
2025-12-04 02:19:00 +0800

    Change slug page content into queryproducts

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -3,33 +3,34 @@
 import Image from 'next/image';
 import { useState } from 'react';
 
-const images = [
-  {
-    id: 1,
-    url: 'https://images.pexels.com/photos/34474293/pexels-photo-34474293.jpeg',
-  },
-  {
-    id: 2,
-    url: 'https://images.pexels.com/photos/34419127/pexels-photo-34419127.jpeg',
-  },
-  {
-    id: 3,
-    url: 'https://images.pexels.com/photos/34615940/pexels-photo-34615940.jpeg',
-  },
-  {
-    id: 4,
-    url: 'https://images.pexels.com/photos/34256053/pexels-photo-34256053.jpeg',
-  },
-];
+// const images = [
+//   {
+//     id: 1,
+//     url: 'https://images.pexels.com/photos/34474293/pexels-photo-34474293.jpeg',
+//   },
+//   {
+//     id: 2,
+//     url: 'https://images.pexels.com/photos/34419127/pexels-photo-34419127.jpeg',
+//   },
+//   {
+//     id: 3,
+//     url: 'https://images.pexels.com/photos/34615940/pexels-photo-34615940.jpeg',
+//   },
+//   {
+//     id: 4,
+//     url: 'https://images.pexels.com/photos/34256053/pexels-photo-34256053.jpeg',
+//   },
+// ];
 
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
   const [index, setIndex] = useState(0);
 
   return (
     <div className="">
       <div className="h-[500px] relative">
         <Image
-          src={images[index].url}
+          key={items.id}
+          src={items[index].image?.url}
           alt=""
           fill
           sizes="30vw"
@@ -37,14 +38,14 @@ const ProductImages = () => {
         ></Image>
       </div>
       <div className="flex justify-between gap-4 mt-8">
-        {images.map((img, i) => (
+        {items.map((item: any, i: number) => (
           <div
             className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
-            key={img.id}
+            key={item.id}
             onClick={() => setIndex(i)}
           >
             <Image
-              src={img.url}
+              src={item.image?.url}
               alt=""
               fill
               sizes="30vw"
